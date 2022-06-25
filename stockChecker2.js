daf0abda5e19d86209acf02a27aec62e6de650c0
Meaza Girma <meazabe3@gmail.com>
2022-06-25 23:36:49 +0300

    new commit to stockChecker2

--- stockChecker2.js
+++ stockChecker2.js
@@ -1,16 +1,31 @@
 /**
  Stock checker - 2
 Use the array of products from the previous exercise. 
-Write a function that returns the products that are available in stock. 
+Write a function that returns the products that are available in stock.  
  */
 
-const products = {
-    prod1: {name: "Towel", price: 25, stockAmount: 0},
-    prod2: {name: "Shower gell", price: 5, stockAmount: 9},
-    prod3: {name: "Shampo", price: 6, stockAmount: 10},
-    prod4: {name: "Conditioner", price: 6, stockAmount: 8},
-    prod5: {name: "Scrub", price: 8, stockAmount: 6},
-    prod6: {name: "Shower cape", price: 3, stockAmount: 2},
-    prod7: {name: "Scruber", price: 7, stockAmount: 0}
-};
-//console.log(products);
+function availableProductsFilter(){
+    const products = [
+        {name: "Towel", price: 25, stockAmount: 0},
+        {name: "Shower gell", price: 5, stockAmount: 9},
+        {name: "Shampo", price: 6, stockAmount: 10},
+        {name: "Conditioner", price: 6, stockAmount: 8},
+        {name: "Scrub", price: 8, stockAmount: 6},
+        {name: "Shower cape", price: 3, stockAmount: 2},
+        {name: "Scruber", price: 7, stockAmount: 0}
+   ];
+    let i = 0;
+    while (i < products.length && products[i].stockAmount > 0){
+        console.log(products[i].name);
+        i++;
+    }
+    
+}
+
+availableProductsFilter();
+
+
+
+
+
+
