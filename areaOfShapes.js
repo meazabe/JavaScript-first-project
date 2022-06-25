90fcf7d75d2f36f342160257c5f7b7e011f170c7
Meaza Girma <meazabe3@gmail.com>
2022-06-25 21:21:44 +0300

    modified areaOfShape.js

--- areaOfShapes.js
+++ areaOfShapes.js
@@ -15,27 +15,25 @@ circle = pi * r * r
 */
 
 
-const triangle = {type: "Triangle", sides: {base: 4, height: 6}};
-            
-const rectangle = {type: "Rectangle", sides: {width: 5, height: 6}};
-
-const circle = {type: "Circle", sides: {pi: 3.1415926535897931, radius: 4}};   
-
-const square = {type: "Square", sides: {side: 6}};
 
 function areaCalculator (shape) {
     let area = 0;
     if (shape.type === "Triangle") {
-        area = 0.5 * (triangle.sides.base * triangle.sides.height);
+        area = 0.5 * (shape.sides.base * shape.sides.height);
     } else if (shape.type === "Rectangle") {
-        area = (rectangle.sides.width) * (rectangle.sides.height);
+        area = (shape.sides.width) * (shape.sides.height);
     } else if (shape.type === "Circle") {
-        area = (circle.sides.pi) * (circle.sides.radius * circle.sides.radius);
+        area = (shape.sides.pi) * (shape.sides.radius * shape.sides.radius);
     } else if (shape.type === "Square") {
-        area = (square.sides.side * square.sides.side);
+        area = (shape.sides.side * shape.sides.side);
     }
     return area;
 }       
+
+const triangle = {type: "Triangle", sides: {base: 4, height: 6}};   
+const rectangle = {type: "Rectangle", sides: {width: 5, height: 6}};
+const circle = {type: "Circle", sides: {pi: 3.1415926535897931, radius: 4}};   
+const square = {type: "Square", sides: {side: 6}};
 
 console.log(areaCalculator(triangle));
 console.log(areaCalculator(rectangle));
