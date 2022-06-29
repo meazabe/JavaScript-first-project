9f778257358c5cd8feb08eedc33a668993b4cdd3
Meaza Girma <meazabe3@gmail.com>
2022-06-29 19:48:21 +0300

    first commit to stockChecker4

--- stockChecker4.js
+++ stockChecker4.js
@@ -7,13 +7,27 @@ Write a function that takes the products and a product name.
 	(The function deletes the given product name if it exists)
  */
 
-    const products = {
-        prod1: {name: "Towel", price: 25, stockAmount: 0},
-        prod2: {name: "Shower gell", price: 5, stockAmount: 9},
-        prod3: {name: "Shampo", price: 6, stockAmount: 10},
-        prod4: {name: "Conditioner", price: 6, stockAmount: 8},
-        prod5: {name: "Scrub", price: 8, stockAmount: 6},
-        prod6: {name: "Shower cape", price: 3, stockAmount: 2},
-        prod7: {name: "Scruber", price: 7, stockAmount: 0}
-    };
-    //console.log(products);
+    const products = [
+        {name: "Towel", price: 25, stockAmount: 4},
+        {name: "Shower gell", price: 5, stockAmount: 0},
+        {name: "Shampo", price: 6, stockAmount: 10},
+        {name: "Conditioner", price: 6, stockAmount: 0},
+        {name: "Scrub", price: 8, stockAmount: 8},
+        {name: "Shower cape", price: 3, stockAmount: 9},
+        {name: "Scruber", price: 7, stockAmount: 2},
+        {name: "hair brush", price: 4, stockAmount: 13}
+    ];
+
+
+   function productOmitter(name){
+        let result = [ ];
+        for (let i = 0; i < products.length; i++){
+            if (products[i].name !== name){
+               result.push(products[i]);
+            }  
+        }
+       return result;
+    }
+    let productName = "hair brush";
+
+   console.log(productOmitter(productName));
