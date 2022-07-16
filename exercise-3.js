e2d8bf019f15fc338b24bdedd0fe1a1df47abf9c
Meaza Girma <meazabe3@gmail.com>
2022-07-16 11:39:29 +0300

    first commit to task1

--- exercise-3.js
+++ exercise-3.js
@@ -1,3 +1,4 @@
+/*  
 function getRecipes() {
     return [
         {
@@ -28,5 +29,61 @@ function getRecipes() {
         }
     ];
 }
+ 
+console.dir(getRecipes(), {depth:null});
+ */
 
-console.log(getRecipes());
+const getRecipes = () => {
+    return [
+        {
+            name: "salmon soup",
+            vegan: false,
+            ingredients: [
+                { name: "salmon",  quantity: 1, }, 
+                { name: "cream", quantity: 2, },
+                { name: "milk", quantity: 1, },
+            ]
+        },
+        {
+            name: "carbonara",
+            vegan: false,
+            ingredients: [
+                { name: "pasta", quantity: 5, },
+                { name: "cheese", quantity: 5, },
+                { name: "milk", quantity: 1, },
+            ]
+        },
+        {
+            name:"rice bowl",
+            vegan: false,
+            ingredients: [
+                { name: "rice", quantity: 2, },
+                { name: "water", quantity: 1, },
+            ]
+        }
+    ];
+}
+
+//console.log(getRecipes());
+//console.dir(getRecipes, {depth:null});
+
+
+//Task: 1. Add a new recipe
+
+const addRecipe = (func, recipe) => {
+    return func.push(recipe); 
+}
+
+let newRecipe = {
+        name: "couscous verde bowl",
+        vegan: true,
+        ingredients: [
+            { name: "salsa verde", quantity: 1, },
+            { name: "whole-wheat couscous", quantity: 2},
+            { name: "corn", quantity: 2, },
+            { name: "black beans", quantity: 1, },
+        ]
+    };
+
+let allRecipes = getRecipes();
+console.dir(addRecipe(allRecipes, newRecipe), {depth:null});
