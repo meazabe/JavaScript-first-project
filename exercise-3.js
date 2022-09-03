9c71df29755a329bb2db018691c7ed5804190b2f
Meaza Girma <meazabe3@gmail.com>
2022-09-03 11:23:25 +0300

    first commit to task 1

--- exercise-3.js
+++ exercise-3.js
@@ -1,38 +1,3 @@
-/*  
-function getRecipes() {
-    return [
-        {
-            name: "salmon soup",
-            vegan: false,
-            ingredients: [
-                { name: "salmon",  quantity: 1, }, 
-                { name: "cream", quantity: 2, },
-                { name: "milk", quantity: 1, },
-            ]
-        },
-        {
-            name: "carbonara",
-            vegan: false,
-            ingredients: [
-                { name: "pasta", quantity: 5, },
-                { name: "cheese", quantity: 5, },
-                { name: "milk", quantity: 1, },
-            ]
-        },
-        {
-            name:"rice bowl",
-            vegan: false,
-            ingredients: [
-                { name: "rice", quantity: 2, },
-                { name: "water", quantity: 1, },
-            ]
-        }
-    ];
-}
- 
-console.dir(getRecipes(), {depth:null});
- */
-
 const getRecipes = () => {
     return [
         {
@@ -67,23 +32,27 @@ const getRecipes = () => {
 //console.log(getRecipes());
 //console.dir(getRecipes, {depth:null});
 
+// Task: 1. Add a new recipe
+//const addRecipe = (recipes, recipe) => {
+    // check if recipe has name and vegan status
+    // add recipe to recipes
+    // return recipes array
+//  }
 
-//Task: 1. Add a new recipe
+const newRecipe = {
+    name: "couscous verde bowl",
+    vegan: true,
+    ingredients: [
+        { name: "salsa verde", quantity: 1, },
+        { name: "whole-wheat couscous", quantity: 2},
+        { name: "corn", quantity: 2, },
+        { name: "black beans", quantity: 1, },
+    ]
+};
 
-const addRecipe = (func, recipe) => {
-    return func.push(recipe); 
-}
+const addRecipe = (recipes, recipe) => {
+        if (recipe.includes("name") && recipe.includes("vegan"))
+        return recipes.concat(recipe);}
 
-let newRecipe = {
-        name: "couscous verde bowl",
-        vegan: true,
-        ingredients: [
-            { name: "salsa verde", quantity: 1, },
-            { name: "whole-wheat couscous", quantity: 2},
-            { name: "corn", quantity: 2, },
-            { name: "black beans", quantity: 1, },
-        ]
-    };
+//console.log(addRecipe(getRecipes(),newRecipe));
 
-let allRecipes = getRecipes();
-console.dir(addRecipe(allRecipes, newRecipe), {depth:null});
